500017601434c38870a9103c4e5bb8aa80be5e3a
unknown <g00376332@gmit.ie>
2021-01-03 15:42:29 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,34 @@
-# flask for web app.
-import flask as fl
-# numpy for numerical work.
+""" 
+Machine Learning and Statistic Project 2020
+server.py
+Student: Maciej Burel
+Student ID: G00376332
+------------------------------------------------------------------------
+
+This server.py is the part of MLS Project.
+
+
+Program is designed to work with folowing model files:
+
+ - neuron.h5
+ - neuron.json
+ - poly.sav
+
+"""
+from flask import Flask
 import numpy as np
+# To resolve issue with keras errors and warnings we have to disable logging messages  
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+import logging
+logging.getLogger('tensorflow').disabled = True
+
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.linear_model import LinearRegression 
+from sklearn.preprocessing import PolynomialFeatures 
+# To import models from file
+import joblib
 
 # Create a new web app.
 app = fl.Flask(__name__)
